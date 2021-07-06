8e7e7a750d206b53e735e6b83165d085a59ae6e5
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-07-06 17:08:13 -0500

    create products and orders tables with a join table, added remainder columns to user table

---
+++ db/migrate/20210706213958_add_products_table.rb
@@ -0,0 +1,10 @@
+class AddProductsTable < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.float :price
+
+      t.timestamps
+    end
+  end
+end
