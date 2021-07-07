d4dd1f8ac8d5fb5a1663bc94d8ae14ae97ab147b
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-07-07 01:04:04 -0500

    run migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   validates :email, presence: true
 
   has_one :access_token, dependent: :destroy
-  has_many :routines, dependent: :destroy
 
   def password
     @password ||= Password.new(encrypted_password) if encrypted_password.present?
