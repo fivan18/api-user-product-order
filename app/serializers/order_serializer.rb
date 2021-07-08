baeeb416d97f71e3d9bdf4bc0c44c67960e6b2f7
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-07-08 11:32:15 -0500

    fix linter errors

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -3,7 +3,7 @@ class OrderSerializer
   attributes :created_at, :products
 
   belongs_to :user, meta: proc { |order, _params|
-    { 
+    {
       first_name: order.user.first_name,
       last_name: order.user.last_name,
       initials: order.user.initials,
