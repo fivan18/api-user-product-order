95ded7b6133e1f361cc71bc2095bdf514fc8c26d
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-07-08 14:19:44 -0500

    fix linter errors

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,22 +1,21 @@
 class DashboardController < ApplicationController
-    before_action :authorize!
-  
-    def index
-      users = UserSerializer.new(User.order(created_at: :desc).first(5))
-      products = ProductSerializer.new(Product.order(created_at: :desc).first(5))
-      orders = Order.order(created_at: :desc).first(5)
-      quantities = Quantity.group(:product_id).sum(:quantity)
+  before_action :authorize!
 
-      sql = "
-        SELECT COUNT('orders'.'id') AS quantity, 
+  def index # rubocop:disable Metrics/MethodLength
+    users = UserSerializer.new(User.order(created_at: :desc).first(5))
+    products = ProductSerializer.new(Product.order(created_at: :desc).first(5))
+    orders = Order.order(created_at: :desc).first(5)
+
+    sql = "
+        SELECT COUNT('orders'.'id') AS quantity,
             strftime('%m', 'orders'.'created_at') || '-' || strftime('%Y', 'orders'.'created_at') AS month
         FROM 'orders'
         GROUP BY month
       "
-      orders_per_month = ActiveRecord::Base.connection.execute(sql)
+    orders_per_month = ActiveRecord::Base.connection.execute(sql)
 
-      sql = "
-        SELECT SUM('quantities'.'quantity') AS quantity, 
+    sql = "
+        SELECT SUM('quantities'.'quantity') AS quantity,
             'quantities'.'product_id' AS id,
             'products'.'name' as name
         FROM 'quantities' JOIN 'products' ON 'quantities'.product_id = 'products'.'id'
@@ -24,8 +23,9 @@ class DashboardController < ApplicationController
         ORDER BY quantity DESC
         LIMIT 5
       "
-      most_sale_products = ActiveRecord::Base.connection.execute(sql)
-  
-      render json: { users: users, products: products, most_sale_products: most_sale_products, orders_per_month: orders_per_month }
-    end
+    most_sale_products = ActiveRecord::Base.connection.execute(sql)
+
+    render json: { users: users, products: products, orders: orders, most_sale_products: most_sale_products,
+                   orders_per_month: orders_per_month }
   end
+end
